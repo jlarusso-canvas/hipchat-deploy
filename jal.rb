60ed6bea2a2677654a47ecacc9914ee00df7872f
Jesse La Russo <jlarusso@canvas.is>
2014-06-11 15:01:42 -0400

    initial

---
+++ jal.rb
@@ -0,0 +1,21 @@
+require 'hipchat'
+
+class Jal
+  def initialize
+    @api_token = ENV['HIPCHAT_API_TOKEN']
+    @client = HipChat::Client.new(@api_token)
+  end
+
+  def send_deploy(msg)
+    @client['technical comms'].send(
+      'Deploy',
+      msg,
+      message_format: 'text',
+      color: 'red'
+    )
+  end
+end
+
+# # Usage
+# j = Jal.new
+# j.send_deploy('/code hi')
